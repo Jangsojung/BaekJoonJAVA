6852e27b409eb425e5b1362be00981348d653325
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2022-06-23 10:26:01 +0900

    Print1000 commit

---
+++ baekjoon/src/baekjoon/Print1000.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Print1000 {
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      /*
+       * BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       */
+      
+      String str = br.readLine();
+      
+      int A = Integer.parseInt(str.split(" ")[0]);
+      int B = Integer.parseInt(str.split(" ")[1]);
+      
+      System.out.println(A + B);
+      
+      /*
+       * bw.write(A + B + "\n");
+       * 
+       * 
+       * bw.flush(); bw.close(); br.close();
+       */
+   }
+
+}
