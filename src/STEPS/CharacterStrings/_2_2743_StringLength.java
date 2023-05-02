f6e49ed9f9c52ee077ca2ba4b4cd145b202d56b3
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2023-05-02 13:45:38 +0900

    18일차 commit

---
+++ src/STEPS/CharacterStrings/_2_2743_StringLength.java
@@ -0,0 +1,13 @@
+package STEPS.CharacterStrings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2_2743_StringLength {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+    }
+}
---
+++ src/STEPS/CharacterStrings/_4_11654_PrintASCII.java
@@ -0,0 +1,13 @@
+package STEPS.CharacterStrings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _4_11654_PrintASCII {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println((int)br.readLine().charAt(0));
+    }
+}
