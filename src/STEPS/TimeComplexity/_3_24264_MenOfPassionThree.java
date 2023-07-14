3128fb42e77b84f649b071400b49451318b98c3d
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2023-07-14 16:40:41 +0900

    63일차 commit

---
+++ src/STEPS/TimeComplexity/_3_24264_MenOfPassionThree.java
@@ -0,0 +1,16 @@
+package STEPS.TimeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _3_24264_MenOfPassionThree {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
