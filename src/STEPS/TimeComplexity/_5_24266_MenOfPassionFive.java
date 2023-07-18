c76ecc9760444e2ed48c773145ca60bf0a940f3c
DESKTOP-DS0U2HC\wkdtj <wkdthwjd99@naver.com>
2023-07-18 16:14:29 +0900

    65일차 commit

---
+++ src/STEPS/TimeComplexity/_5_24266_MenOfPassionFive.java
@@ -0,0 +1,16 @@
+package STEPS.TimeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _5_24266_MenOfPassionFive {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
